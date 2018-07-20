93af8ddffa41637a21a63f565900c0aa36b1b764
Harbour <864353016@qq.com>
2018-07-20 17:16:47 +0800

    修改程序集版本号

--- Harbour.Utils/Properties/AssemblyInfo.cs
+++ Harbour.Utils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Harbour.Utilities")]
-[assembly: AssemblyDescription("Harbour.Utilities")]
+[assembly: AssemblyTitle("Harbour.Utils")]
+[assembly: AssemblyDescription("Harbour.Utils")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Harbour<864353016@qq.com>")]
-[assembly: AssemblyProduct("Harbour.Utilities")]
+[assembly: AssemblyProduct("Harbour.Utils")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("Harbour<864353016@qq.com>")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
