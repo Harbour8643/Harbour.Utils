8b88312d81bfea8e9c81d0354f3d9dcc127b4729
Harbour <864353016@qq.com>
2019-05-22 16:53:47 +0800

    版本升级

--- net40/Harbour.Utils.Extension/Properties/AssemblyInfo.cs
+++ net40/Harbour.Utils.Extension/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Harbour<864353016@qq.com>")]
 [assembly: AssemblyProduct("Harbour.Utils.Extension")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("Harbour<864353016@qq.com>")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
